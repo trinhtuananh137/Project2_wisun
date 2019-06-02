d9629b86f7494eedfd5ca1578d82dca207b12ae9
Vincent Leo <40976212+trinhtuananh137@users.noreply.github.com>
2019-06-03 00:33:59 +0700

    Update

--- Wisun_SEND.py
+++ Wisun_SEND.py
@@ -1,10 +1,8 @@
 import serial
 import base64
-import cv2
-import numpy as np
-from PIL import Image
 from serial import Serial
-from time import sleep
+import os
+import cv2
 
 CORDIP = 'FE80:0000:0000:0000:021D:1290:0004:631F'
 
@@ -19,40 +17,48 @@ def d2h(x):
     elif(_len ==3):
         _str = "0" + str
     return(_str)
-image_path = "./face.jpg"
-imageFile = open(image_path, "rb")
-bstr = base64.b64encode(imageFile.read())
-_str = bstr.decode()
-imageFile.close()
-_len  = len(_str)
-m = int(_len/200) * 200
-send = []
-for i in range(0,m + 1,200):
-    send.append(_str[0 + i:200 +i])
+def Wisun_SEND(image_path):
+    while cv2.waitKey(30)&0xFF != ord('q'):
+        if not(os.path.exists(image_path)):
+            continue
+        try:
+            imageFile = open(image_path, "rb")
+        except OSError:
+            continue
+        _str = base64.b64encode(imageFile.read())
+        ser_send = serial.Serial(    
+            port='COM5',
+            baudrate=115200,
+            timeout = 10,
+            parity = serial.PARITY_NONE,
+            bytesize=serial.EIGHTBITS,
+            rtscts = True,
+            dsrdtr = False
+        )
+        #image_path = "./anh/anh.jpg"
+        
+        #_str = bstr.decode()
+        imageFile.close()
+        _len  = len(_str)
+        m = int(_len/200) * 200
+        send = []
+        for i in range(0,m + 1,200):
+            send.append(_str[0 + i:200 +i])
+        for s in send:
+            l = ''
+            l = l + d2h(len(s))
+            cmd = "SKSENDTO 1 "+ CORDIP +" 0E1A 1 " + l
+            ser_send.write(cmd.encode('ascii')  + b" " + s + b'\r\n') 
+            mes_send = ser_send.readline()    
+            while not(b'OK' in mes_send):
+                mes_send = ser_send.readline()
+        cmd = "SKSENDTO 1 "+ CORDIP +" 0E1A 1 0003 end\r\n"
+        ser_send.write(cmd.encode('ascii'))
+        mes_send = ser_send.readline()    
+        while not(b'OK' in mes_send):
+            mes_send = ser_send.readline()
+        ser_send.close()
+        imageFile.close()
 
-ser_send = serial.Serial(    
-    port='COM5',
-    baudrate=115200,
-    timeout = 10,
-    parity = serial.PARITY_NONE,
-    bytesize=serial.EIGHTBITS,
-    rtscts = True,
-    dsrdtr = False
-)
 
-# cmd = "SKSENDTO 1 "+ CORDIP +" 0E1A 1 0005 begin\r\n"
-# ser_send.write(cmd.encode('ascii'))
-
-for s in send:
-    l = ''
-    l = l + d2h(len(s))
-    cmd = "SKSENDTO 1 "+ CORDIP +" 0E1A 1 " + l + " " + s + '\r\n'
-    ser_send.write(cmd.encode('ascii')) 
-    mes_send = ser_send.readline()    
-    while not(b'OK' in mes_send):
-        mes_send = ser_send.readline()
-cmd = "SKSENDTO 1 "+ CORDIP +" 0E1A 1 0003 end\r\n"
-ser_send.write(cmd.encode('ascii'))
-mes_send = ser_send.readline()    
-while not(b'OK' in mes_send):
-    mes_send = ser_send.readline()
+        
