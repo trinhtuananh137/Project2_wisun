3246d37fe7b58fa6e5dc620537a8eefacdc45153
Vincent Leo <40976212+trinhtuananh137@users.noreply.github.com>
2019-06-11 15:47:50 +0700

    update begin - end when receive

--- Wisun_RECEIVE.py
+++ Wisun_RECEIVE.py
@@ -15,18 +15,38 @@ ser_receive = serial.Serial(
     rtscts = True,
     dsrdtr = False
 )
-receive = ''
-mes_receive = ser_receive.readline()
-if os.path.isfile("imageToSave.png"):
-        os.remove("imageToSave.png")
-while not(b'end' in mes_receive):    
-    fh = open("imageToSave.png", "ab")
-    if mes_receive != b'':
-        a = mes_receive.decode()
-        b = a[121:]
-        c = b.encode()
-        fh.write(base64.b64decode(c))
+
+while 1:
     mes_receive = ser_receive.readline()
-fh.close()
+    if not(b"begin" in mes_receive):
+        continue
+    else:
+        mes_receive = ser_receive.readline()
+    if os.path.isfile("imageToSave.png"):
+        os.remove("imageToSave.png")
+    fh = open("imageToSave.png","ab")
+    while not(b'end' in mes_receive):    
+        if mes_receive != b'':
+            a = mes_receive[121:]
+            #b = a[121:]
+            #c = b.encode()
+            try:
+                fh.write(base64.b64decode(a))
+            except:
+                break
+        mes_receive = ser_receive.readline()
+    #mes_receive = ser_receive.readline()
+    fh.close()
+    if os.path.isfile("imageToSave.png"):    
+        fh = open("imageToSave.png", "rb")
+        bfh = fh.read()
+        if bfh != b"":
+            fh_proc = open("imageToProcess.png","wb")
+            fh_proc.write(bfh)
+            fh_proc.close()
+        fh.close()    
+    mes_receive = ser_receive.readline()
+    #os.remove("imageToSave.png")
+
         
     
